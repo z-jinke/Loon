2903a1244f045fef62ba937a324a7f9ec7f48e6f
Z <155460219+z-jinke@users.noreply.github.com>
2025-05-04 17:23:18 +0800

    更新picsart.js

--- Script/picsart.js
+++ Script/picsart.js
@@ -1,46 +1,16 @@
-//更新于2025.4.8
+// 更新于 2025.5.4
+
 let url = $request.url;
 let body = $response.body;
-
-if (/^https:\/\/api\.aidimension\.cn\/shop\/subscription\/apple\/purchases/.test(url)) {
-    let obj = JSON.parse(body);
-    obj.status = "success"; 
-
-    obj.response = [
-        {
-            "status": "SUBSCRIPTION_PURCHASED",
-            "order_id": "490001314520000",
-            "original_order_id": "490001314520000",
-            "is_trial": true,
-            "plan_meta": {
-                "storage_limit_in_mb": 20480,
-                "frequency": "yearly",
-                "scope_id": "full",
-                "id": "com.picsart.editor.subscription_yearly",
-                "product_id": "subscription_yearly",
-                "level": 2000,
-                "auto_renew_product_id": "com.picsart.editor.subscription_yearly",
-                "type": "renewable",
-                "tier_id": "gold_old",
-                "permissions": [
-                    "premium_tools_standard",
-                    "premium_tools_ai"
-                ],
-                "description": "china"
-            },
-            "limitation": {
-                "max_count": 5,
-                "limits_exceeded": false
-            },
-            "reason": "ok",
-            "subscription_id": "com.picsart.editor.subscription_yearly",
-            "is_eligible_for_introductory": false,
-            "purchase_date": 1687020148000,
-            "expire_date": 4092599349000
-        }
-    ];
-
-    body = JSON.stringify(obj);
+try {
+    if (/^https:\/\/api\.aidimension\.cn\/shop\/subscription\/apple\/purchases/.test(url)) {
+        let obj = JSON.parse(body);
+        obj.status = "success";
+        obj.response = [{"status":"SUBSCRIPTION_PURCHASED","order_id":"490001314520000","original_order_id":"490001314520000","is_trial":true,"plan_meta":{"storage_limit_in_mb":20480,"frequency":"yearly","scope_id":"full","id":"com.picsart.editor.subscription_yearly","product_id":"subscription_yearly","level":2000,"auto_renew_product_id":"com.picsart.editor.subscription_yearly","type":"renewable","tier_id":"gold_old","permissions":["premium_tools_standard","premium_tools_ai"],"description":"china"},"limitation":{"max_count":5,"limits_exceeded":false},"reason":"ok","subscription_id":"com.picsart.editor.subscription_yearly","is_eligible_for_introductory":false,"purchase_date":1687020148000,"expire_date":4092599349000}];
+        body = JSON.stringify(obj);
+    }
+    $done({ body });
+} catch (e) {
+    $notify("Picsart 脚本运行错误", `请求URL: ${url}`, `错误信息: ${e.message}`);
+    $done({ body });
 }
-
-$done({ body });
